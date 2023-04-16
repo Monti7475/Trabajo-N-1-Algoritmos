6a114e75bd3d5cb450cb5d2fa728563c06f6e7f8
Monti7475 <cristobal.montiel@uc.cl>
2023-04-16 15:09:59 -0400

    .gitignore añadido y modificacion del readme y main.py

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Trabajo-N-1-Algoritmos
+**Algoritmos Sistem Computacionales** curso **IIC1001-1**
+## Qué es esto?	
+
+Esto es una prueba del uso de **Git** y **Github**
+
+## Que archivos incluyo?
+- un *.gitignore* que muestra que archivos **ignorar**
+- un *main.py* que contiene codigo python
+- este *readme.md* para probar el formato **markdow**
+
+
+**Cristobal Montiel**
---
+++ main.py
@@ -0,0 +1,26 @@
+print("""This one is for my one and only true love
+Princess Peach
+
+Peach, you're so cool
+And with my star, we're gonna rule
+Peach, understand
+I'm gonna love you till the very end
+
+Peaches, Peaches, Peaches, Peaches, Peaches
+Peaches, Peaches, Peaches, Peaches, Peaches
+I love you, oh
+Peaches, Peaches, Peaches, Peaches, Peaches
+Peaches, Peaches, Peaches, Peaches, Peaches
+I love you, oh
+
+Mario, Luigi and a Donkey Kong too
+A thousand troops of Koopas couldn't keep me from you
+Princess Peach, at the end of the line
+I'll make you mine, oh
+
+Peaches, Peaches, Peaches, Peaches, Peaches
+Peaches, Peaches, Peaches, Peaches, Peaches
+I love you, oh
+
+Peaches, Peaches
+Peach, Peach""")
